a425ea88c2853a5580761a84ff2a934d196d1c96
anthonybourdeau <tonyr2004@neuf.fr>
2024-02-01 17:18:48 +0100

    new commit 17:18

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -6,17 +6,6 @@ use Poo\Project\Kernel\AbstractController;
 use Poo\Project\Kernel\Model;
 
 class GenreController extends AbstractController{
- // affichage des genres
- /*
- public function displayGenres()
- {
-     $model = Model::getInstance();
-     $result = $model->readAll('genre');
-     $genreResult = ['genres' => $result];
-     $this->render('genres', 'Nos livres par Genres', $genreResult);
-
- }
-*/
 
     public function Genre()
     {
