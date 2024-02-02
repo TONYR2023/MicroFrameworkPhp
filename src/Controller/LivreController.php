4262bcd09f89c791671a33fec7e01747ece882fd
anthonybourdeau <tonyr2004@neuf.fr>
2024-02-02 21:26:39 +0100

     changement Genrecontoller et genre.php

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -57,5 +57,4 @@ class LivreController extends AbstractController
         $this->render('livres', 'Mes livres', ['livres' => $livres]);
     }
 
-   
 }
--- src/Views/search/index.php
+++ src/Views/search/index.php
@@ -1,1 +1,11 @@
-<p>test</p>
+<div class="card">
+  <div class="card-body">
+    <div class="" > <h2>Bienvenue sur notre site MICRO-FRAMEWORK</h2></div>
+  </div>
+</div>
+<div class="d-flex gap-2 list-unstyled align-items-center justify-content-center">
+                        <a class="d-flex btn btn-dark" role="search" href="?controller=LivreController&method=livre">Rechercher des livres</a>
+                </div>
+                <div class="d-flex gap-2 list-unstyled align-items-center justify-content-center">
+                        <a class="d-flex btn btn-dark" role="search" href="?controller=GenreController&method=genre">Rechercher livres par genres</a>
+                </div>
